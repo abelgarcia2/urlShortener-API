39dd60d31dcf2adc71e03d5b624fcbf039db8c7c
abelgarcia2 <65343232+abelgarcia2@users.noreply.github.com>
2021-12-15 14:52:51 +0100

    Intentando hacer alerta

--- frontend/index.js
+++ frontend/index.js
@@ -1,11 +1,12 @@
 const btn = document.getElementById('btn')
 btn.addEventListener('click', () => {
     const url = document.getElementById('url').value
-    btn.disabled = true
+    console.log(url);
+    // btn.disabled = true
     if (/(^https?:\/\/.)|(^www\..)|(^[A-Za-z]+\..)/.test(url)) {
         fetch('http://localhost:5000/shorten', { method: 'POST', body: JSON.stringify({ url: url }), headers: { 'Content-Type': 'application/json' } })
             .then(data => data.json()).then(data => {
-                btn.disabled = false
+                // btn.disabled = false
                 const shortUrl = document.getElementById('shortURL')
                 //TODO: quitar console
                 console.log(data)
@@ -15,7 +16,7 @@ btn.addEventListener('click', () => {
                 document.getElementById('copyClipboard').addEventListener('click', () => navigator.clipboard.writeText(data.url))
             })
     } else {
-        console.log('NO')
+        alert('NO')
     }
 
 })
