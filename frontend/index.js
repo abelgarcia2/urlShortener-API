ddab996ed63efde3b1c571a984fdc3b606171b80
abelgarcia2 <65343232+abelgarcia2@users.noreply.github.com>
2021-12-17 20:58:17 +0100

    First version finished

--- frontend/index.js
+++ frontend/index.js
@@ -1,22 +1,36 @@
 const btn = document.getElementById('btn')
 btn.addEventListener('click', () => {
     const url = document.getElementById('url').value
+    const alert = document.getElementById('alert')
+    const result = document.getElementById('result')
+    const loader = document.getElementById('loader')
+
     console.log(url);
-    // btn.disabled = true
+    btn.disabled = true
+    result.style.display = 'none'
+    loader.style.display = 'block'
     if (/(^https?:\/\/.)|(^www\..)|(^[A-Za-z]+\..)/.test(url)) {
         fetch('http://localhost:5000/shorten', { method: 'POST', body: JSON.stringify({ url: url }), headers: { 'Content-Type': 'application/json' } })
             .then(data => data.json()).then(data => {
-                // btn.disabled = false
+                btn.disabled = false
                 const shortUrl = document.getElementById('shortURL')
                 //TODO: quitar console
                 console.log(data)
-                document.getElementById('result').style.display = 'block'
+                result.style.display = 'block'
+                loader.style.display = 'none'
                 shortUrl.href = data.url
                 shortUrl.innerHTML = data.url.replace('https://', '')
                 document.getElementById('copyClipboard').addEventListener('click', () => navigator.clipboard.writeText(data.url))
             })
     } else {
-        alert('NO')
+        result.style.display = 'none'
+        loader.style.display = 'none'
+        alert.style.display = 'block'
+        btn.disabled = false
+        setTimeout(() => {
+            alert.style.display = 'none'
+        }, 2000)
+        console.log('NO')
     }
 
 })
