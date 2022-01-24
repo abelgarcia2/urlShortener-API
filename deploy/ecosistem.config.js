d3f4fd239df6836de90458e5f7c6b48ec1eeb32d
abelgarcia2 <65343232+abelgarcia2@users.noreply.github.com>
2022-01-24 23:11:29 +0100

    Create deploy folder

---
+++ deploy/ecosistem.config.js
@@ -0,0 +1,10 @@
+export const apps = [
+    name, 'urlShortener',
+    script, 'npm run start',
+    instances, 3,
+    watch, true,
+    increment_var, 'PORT',
+    env, {
+        'PORT': 5000,
+    }
+];
