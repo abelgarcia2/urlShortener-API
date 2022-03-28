eec9344c40c0aa47a2c076064f24a8a135ecd4d7
abelgarcia2 <65343232+abelgarcia2@users.noreply.github.com>
2022-03-28 12:22:07 +0200

    Added cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import './db.js';
+import cors from 'cors'
 import apiRoutes from './routes/api.routes.js';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 app.get('/', (req, res) => res.json({ 'msg': 'Welcome to shortLink' }));
 
