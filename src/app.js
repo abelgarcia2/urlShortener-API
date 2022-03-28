acd1dd605e8c02b2966f333a92de62930f12d909
abelgarcia2 <65343232+abelgarcia2@users.noreply.github.com>
2022-03-28 12:12:27 +0200

    Deleted morgan from the code

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 import express from 'express';
-import morgan from 'morgan';
 import './db.js';
 import apiRoutes from './routes/api.routes.js';
 
 const app = express();
 
 app.use(express.json());
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
 app.get('/', (req, res) => res.json({ 'msg': 'Welcome to shortLink' }));
 
